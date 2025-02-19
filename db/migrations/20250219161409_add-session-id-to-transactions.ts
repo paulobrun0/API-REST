3932581614e3090d1e12f710c03790951ae8d9ae
paulobrun0 <paulo.bruno04@hotmail.com>
2025-02-19 14:22:50 -0300

    Banco de dados - Tratando env com Zod

--- db/migrations/20250219161409_add-session-id-to-transactions.ts
+++ db/migrations/20250219161409_add-session-id-to-transactions.ts
@@ -1,8 +1,6 @@
 import type { Knex } from 'knex'
 
-export async function up(knex: Knex): Promise<void> {
-
-}
+export async function up(knex: Knex): Promise<void> {}
 
 export async function down(knex: Knex): Promise<void> {
   await knex.schema.alterTable('transactions', (table) => {
---
+++ src/env/index.ts
@@ -0,0 +1,20 @@
+import 'dotenv/config'
+import { z } from 'zod'
+
+export const envSchema = z.object({
+  NODE_ENV: z
+    .enum(['development', 'production', 'test'])
+    .default('development'),
+  DATABASE_URL: z.string(),
+  PORT: z.number().default(3333),
+})
+
+const _env = envSchema.safeParse(process.env)
+
+if (_env.success === false) {
+  console.error('❌ Invalid environment variables', _env.error.flatten())
+
+  throw new Error('Invalid environment variables')
+}
+
+export const env = _env.data
